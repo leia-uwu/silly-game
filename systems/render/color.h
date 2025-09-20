31d7f0f3f1c150901e08cf752e69154415c458a0
leia uwu <leia@tutamail.com>
2025-09-20 15:15:13 -0300

    feat: more Color class methods

--- systems/render/color.h
+++ systems/render/color.h
@@ -23,4 +23,38 @@ public:
         b((hex) & 0xFF)
     {
     }
+
+    [[nodiscard]] uint32_t RGBHex() const
+    {
+        return r | (g << 8) | (b << 16);
+    }
+
+    [[nodiscard]] uint32_t RGBAHex() const
+    {
+        return r | (g << 8) | (b << 16) | (a << 24);
+    }
+
+    //
+    // for values between 0 and 1
+    //
+
+    [[nodiscard]] float normalizedR() const
+    {
+        return r / 255.F;
+    }
+
+    [[nodiscard]] float normalizedG() const
+    {
+        return g / 255.F;
+    }
+
+    [[nodiscard]] float normalizedB() const
+    {
+        return b / 255.F;
+    }
+
+    [[nodiscard]] float normalizedA() const
+    {
+        return a / 255.F;
+    }
 };
