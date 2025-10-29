741d281f6045c9da8eb2bcae36d46bfd8029f434
leia uwu <leia@tutamail.com>
2025-10-28 23:39:04 -0300

    refactor: clean up grid.h

--- systems/include/math/grid.h
+++ systems/include/math/grid.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "math/vec2.h"
 
 #include <algorithm>
@@ -11,29 +13,14 @@ template<typename GridSize_T, typename EntityID_T>
 class Grid
 {
 public:
-    Grid(GridSize_T worldSize, GridSize_T cellSize, EntityID_T maxEntityID) :
-        m_worldSize(worldSize),
-        m_cellSize(cellSize),
-        m_gridSize(worldSize / cellSize),
-        m_cellCount(m_gridSize * m_gridSize),
-        m_maxEntityID(maxEntityID)
-    {
-        m_cells = new Cell[m_cellCount];
-        m_entityCache = new EntityGridData[maxEntityID];
-
-        m_resultCache.reserve(256);
-    }
+    Grid(GridSize_T worldSize, GridSize_T cellSize, EntityID_T maxEntityID);
 
     Grid(const Grid&) = delete;
     Grid(Grid&&) = delete;
     Grid& operator=(const Grid&) = delete;
     Grid& operator=(Grid&&) = delete;
 
-    ~Grid()
-    {
-        delete[] m_cells;
-        delete[] m_entityCache;
-    }
+    ~Grid();
 
     [[nodiscard]] float worldSize() const
     {
@@ -55,178 +42,17 @@ public:
         return m_maxEntityID;
     }
 
-    void insertEntity(EntityID_T entityID, Vec2 min, Vec2 max)
-    {
-        EntityGridData& entity = getEntityData(entityID);
+    void insertEntity(EntityID_T entityID, Vec2 min, Vec2 max);
 
-        GridAABB localBounds = {
-            .min = roundToGrid(min),
-            .max = roundToGrid(max),
-        };
+    void removeEntity(EntityID_T entityID);
 
-        // entity already exists
-        if (entity.valid) {
-            GridAABB& bounds = entity.bounds;
+    const std::vector<EntityID_T>& queryAABB(Vec2 min, Vec2 max) const;
 
-            // if the bounds didn't change from the existing ones return earlier
-            if (
-                localBounds.min == bounds.min
-                && localBounds.max == bounds.max
-            ) {
-                return;
-            }
+    const std::vector<EntityID_T>& queryPosition(Vec2 pos) const;
 
-            if (bounds.min == bounds.max) {
-                Cell& cell = cellAt(bounds.min.x, bounds.min.y);
-                auto it = std::find(cell.items.begin(), cell.items.end(), entityID);
-                if (it != cell.items.end()) {
-                    cell.items.erase(it);
-                }
-            } else {
-                for (GridSize_T y = bounds.min.y; y <= bounds.max.y; y++) {
-                    for (GridSize_T x = bounds.min.x; x <= bounds.max.x; x++) {
-                        Cell& cell = cellAt(x, y);
-                        auto it = std::find(cell.items.begin(), cell.items.end(), entityID);
-                        if (it != cell.items.end()) {
-                            cell.items.erase(it);
-                        }
-                    }
-                }
-            }
-        }
+    const std::vector<EntityID_T>& queryEntity(EntityID_T entityID) const;
 
-        entity.valid = true;
-        entity.bounds = localBounds;
-
-        for (GridSize_T y = localBounds.min.y; y <= localBounds.max.y; y++) {
-            for (GridSize_T x = localBounds.min.x; x <= localBounds.max.x; x++) {
-                Cell& cell = cellAt(x, y);
-                cell.items.push_back(entityID);
-            }
-        }
-    }
-
-    void removeEntity(EntityID_T entityID)
-    {
-        EntityGridData& entity = getEntityData(entityID);
-
-        if (!entity.valid)
-            return;
-
-        GridAABB& bounds = entity.bounds;
-
-        // erase entity from current grid cells its occupying
-        if (bounds.min == bounds.max) {
-            Cell& cell = cellAt(bounds.min.x, bounds.min.y);
-            auto it = std::find(cell.items.begin(), cell.items.end(), entityID);
-            if (it != cell.items.end()) {
-                cell.items.erase(it);
-            }
-        } else {
-            for (GridSize_T y = bounds.min.y; y <= bounds.max.y; y++) {
-                for (GridSize_T x = bounds.min.x; x <= bounds.max.x; x++) {
-                    Cell& cell = cellAt(x, y);
-                    auto it = std::find(cell.items.begin(), cell.items.end(), entityID);
-                    if (it != cell.items.end()) {
-                        cell.items.erase(it);
-                    }
-                }
-            }
-        }
-
-        entity.valid = false;
-        entity.queryID = 0;
-        entity.bounds = {{0, 0}, {0, 0}};
-    }
-
-    const std::vector<EntityID_T>& queryAABB(Vec2 min, Vec2 max)
-    {
-        GridAABB bounds = {
-            .min = roundToGrid(min),
-            .max = roundToGrid(max),
-        };
-
-        return queryGridAABB(bounds);
-    }
-
-    const std::vector<EntityID_T>& queryPosition(Vec2 pos)
-    {
-        GridPos gridPos = roundToGrid(pos);
-        return cellAt(gridPos.x, gridPos.y).items;
-    }
-
-    const std::vector<EntityID_T>& queryEntity(EntityID_T entityID)
-    {
-        EntityGridData& entity = m_entityCache[entityID];
-        assert(entity.valid);
-        return queryGridAABB(entity.bounds);
-    }
-
-    const std::vector<EntityID_T>& queryLine(Vec2 lineStart, Vec2 lineEnd)
-    {
-        Vec2 diff = lineEnd - lineStart;
-
-        int gridDirX = lineEnd.x >= lineStart.x ? 1 : -1;
-        int gridDirY = lineEnd.y >= lineStart.y ? 1 : -1;
-
-        float dirX =
-            std::abs(diff.x) > 0.00001
-            ? (gridDirX * (int)m_cellSize) / diff.x
-            : DBL_MAX;
-
-        float dirY =
-            std::abs(diff.y) > 0.00001
-            ? (gridDirY * (int)m_cellSize) / diff.y
-            : DBL_MAX;
-
-        // cell relative
-        float relativeX = std::fmod(lineStart.x / m_cellSize, 1.F);
-        float relativeY = std::fmod(lineStart.y / m_cellSize, 1.F);
-
-        float x = dirX * (gridDirX > 0 ? 1.F - relativeX : relativeX);
-        float y = dirY * (gridDirY > 0 ? 1.F - relativeY : relativeY);
-
-        GridPos start = roundToGrid(lineStart);
-        GridPos endCell = roundToGrid(lineEnd);
-
-        int cellX = start.x;
-        int cellY = start.y;
-
-        uint32_t query = getNextQueryID();
-        m_resultCache.clear();
-
-        while (true) {
-            Cell& cell = cellAt(cellX, cellY);
-
-            for (EntityID_T entityId : cell.items) {
-                EntityGridData& entity = getEntityData(entityId);
-                if (entity.queryID != query) {
-                    entity.queryID = query;
-                    m_resultCache.push_back(entityId);
-                }
-            }
-
-            if (cellX == endCell.x && cellY == endCell.y) {
-                break;
-            }
-
-            if (x < y) {
-                x += dirX;
-                cellX += gridDirX;
-                if (cellX < 0 || cellX >= m_gridSize) {
-                    break;
-                }
-            } else {
-                y += dirY;
-                cellY += gridDirY;
-                if (cellY < 0 || cellY >= m_gridSize) {
-                    break;
-                }
-            }
-        }
-
-        return m_resultCache;
-    }
+    const std::vector<EntityID_T>& queryLine(Vec2 lineStart, Vec2 lineEnd) const;
 
 private:
     struct Cell
@@ -262,7 +88,7 @@ private:
         GridAABB bounds;
     };
 
-    GridPos roundToGrid(Vec2 pos)
+    GridPos roundToGrid(Vec2 pos) const
     {
         return {
             .x = (GridSize_T)std::clamp((int)(pos.x / m_cellSize), 0, (int)m_gridSize - 1),
@@ -270,7 +96,7 @@ private:
         };
     }
 
-    const std::vector<EntityID_T>& queryGridAABB(GridAABB bounds)
+    const std::vector<EntityID_T>& queryGridAABB(GridAABB bounds) const
     {
         m_resultCache.clear();
 
@@ -314,7 +140,7 @@ private:
     size_t m_cellCount;
     Cell* m_cells;
 
-    Cell& cellAt(GridSize_T x, GridSize_T y)
+    Cell& cellAt(GridSize_T x, GridSize_T y) const
     {
         size_t idx = (y * m_gridSize) + x;
         assert(idx <= m_cellCount);
@@ -327,7 +153,7 @@ private:
     EntityID_T m_maxEntityID;
     EntityGridData* m_entityCache;
 
-    EntityGridData& getEntityData(EntityID_T ID)
+    EntityGridData& getEntityData(EntityID_T ID) const
     {
         assert(ID <= m_maxEntityID);
         return m_entityCache[ID];
@@ -336,10 +162,210 @@ private:
     /**
      * Cached vector for query results to avoid allocations
      */
-    std::vector<EntityID_T> m_resultCache;
-    uint32_t m_nextQueryID = 1;
-    uint32_t getNextQueryID()
+    mutable std::vector<EntityID_T> m_resultCache;
+    mutable uint32_t m_nextQueryID = 1;
+    uint32_t getNextQueryID() const
     {
         return m_nextQueryID++;
     }
 };
+
+template<typename GridSize_T, typename EntityID_T>
+inline Grid<GridSize_T, EntityID_T>::Grid(GridSize_T worldSize, GridSize_T cellSize, EntityID_T maxEntityID) :
+    m_worldSize(worldSize),
+    m_cellSize(cellSize),
+    m_gridSize(worldSize / cellSize),
+    m_cellCount(m_gridSize * m_gridSize),
+    m_maxEntityID(maxEntityID)
+{
+    m_cells = new Cell[m_cellCount];
+    m_entityCache = new EntityGridData[maxEntityID];
+
+    m_resultCache.reserve(256);
+}
+
+template<typename GridSize_T, typename EntityID_T>
+inline Grid<GridSize_T, EntityID_T>::~Grid()
+{
+    delete[] m_cells;
+    delete[] m_entityCache;
+}
+
+template<typename GridSize_T, typename EntityID_T>
+inline void Grid<GridSize_T, EntityID_T>::insertEntity(EntityID_T entityID, Vec2 min, Vec2 max)
+{
+    EntityGridData& entity = getEntityData(entityID);
+
+    GridAABB localBounds = {
+        .min = roundToGrid(min),
+        .max = roundToGrid(max),
+    };
+
+    // entity already exists
+    if (entity.valid) {
+        GridAABB& bounds = entity.bounds;
+
+        // if the bounds didn't change from the existing ones return earlier
+        if (
+            localBounds.min == bounds.min
+            && localBounds.max == bounds.max
+        ) {
+            return;
+        }
+
+        if (bounds.min == bounds.max) {
+            Cell& cell = cellAt(bounds.min.x, bounds.min.y);
+            auto it = std::find(cell.items.begin(), cell.items.end(), entityID);
+            if (it != cell.items.end()) {
+                cell.items.erase(it);
+            }
+        } else {
+            for (GridSize_T y = bounds.min.y; y <= bounds.max.y; y++) {
+                for (GridSize_T x = bounds.min.x; x <= bounds.max.x; x++) {
+                    Cell& cell = cellAt(x, y);
+                    auto it = std::find(cell.items.begin(), cell.items.end(), entityID);
+                    if (it != cell.items.end()) {
+                        cell.items.erase(it);
+                    }
+                }
+            }
+        }
+    }
+
+    entity.valid = true;
+    entity.bounds = localBounds;
+
+    for (GridSize_T y = localBounds.min.y; y <= localBounds.max.y; y++) {
+        for (GridSize_T x = localBounds.min.x; x <= localBounds.max.x; x++) {
+            Cell& cell = cellAt(x, y);
+            cell.items.push_back(entityID);
+        }
+    }
+}
+
+template<typename GridSize_T, typename EntityID_T>
+inline void Grid<GridSize_T, EntityID_T>::removeEntity(EntityID_T entityID)
+{
+    EntityGridData& entity = getEntityData(entityID);
+
+    if (!entity.valid)
+        return;
+
+    GridAABB& bounds = entity.bounds;
+
+    // erase entity from current grid cells its occupying
+    if (bounds.min == bounds.max) {
+        Cell& cell = cellAt(bounds.min.x, bounds.min.y);
+        auto it = std::find(cell.items.begin(), cell.items.end(), entityID);
+        if (it != cell.items.end()) {
+            cell.items.erase(it);
+        }
+    } else {
+        for (GridSize_T y = bounds.min.y; y <= bounds.max.y; y++) {
+            for (GridSize_T x = bounds.min.x; x <= bounds.max.x; x++) {
+                Cell& cell = cellAt(x, y);
+                auto it = std::find(cell.items.begin(), cell.items.end(), entityID);
+                if (it != cell.items.end()) {
+                    cell.items.erase(it);
+                }
+            }
+        }
+    }
+
+    entity.valid = false;
+    entity.queryID = 0;
+    entity.bounds = {{0, 0}, {0, 0}};
+}
+
+template<typename GridSize_T, typename EntityID_T>
+inline const std::vector<EntityID_T>& Grid<GridSize_T, EntityID_T>::queryAABB(Vec2 min, Vec2 max) const
+{
+    GridAABB bounds = {
+        .min = roundToGrid(min),
+        .max = roundToGrid(max),
+    };
+
+    return queryGridAABB(bounds);
+}
+
+template<typename GridSize_T, typename EntityID_T>
+inline const std::vector<EntityID_T>& Grid<GridSize_T, EntityID_T>::queryPosition(Vec2 pos) const
+{
+    GridPos gridPos = roundToGrid(pos);
+    return cellAt(gridPos.x, gridPos.y).items;
+}
+
+template<typename GridSize_T, typename EntityID_T>
+inline const std::vector<EntityID_T>& Grid<GridSize_T, EntityID_T>::queryEntity(EntityID_T entityID) const
+{
+    EntityGridData& entity = m_entityCache[entityID];
+    assert(entity.valid);
+    return queryGridAABB(entity.bounds);
+}
+
+template<typename GridSize_T, typename EntityID_T>
+inline const std::vector<EntityID_T>& Grid<GridSize_T, EntityID_T>::queryLine(Vec2 lineStart, Vec2 lineEnd) const
+{
+    Vec2 diff = lineEnd - lineStart;
+
+    int gridDirX = lineEnd.x >= lineStart.x ? 1 : -1;
+    int gridDirY = lineEnd.y >= lineStart.y ? 1 : -1;
+
+    float dirX =
+        std::abs(diff.x) > 0.00001
+        ? (gridDirX * (int)m_cellSize) / diff.x
+        : DBL_MAX;
+
+    float dirY =
+        std::abs(diff.y) > 0.00001
+        ? (gridDirY * (int)m_cellSize) / diff.y
+        : DBL_MAX;
+
+    // cell relative
+    float relativeX = std::fmod(lineStart.x / m_cellSize, 1.F);
+    float relativeY = std::fmod(lineStart.y / m_cellSize, 1.F);
+
+    float x = dirX * (gridDirX > 0 ? 1.F - relativeX : relativeX);
+    float y = dirY * (gridDirY > 0 ? 1.F - relativeY : relativeY);
+
+    GridPos start = roundToGrid(lineStart);
+    GridPos endCell = roundToGrid(lineEnd);
+
+    int cellX = start.x;
+    int cellY = start.y;
+
+    uint32_t query = getNextQueryID();
+    m_resultCache.clear();
+
+    while (true) {
+        Cell& cell = cellAt(cellX, cellY);
+
+        for (EntityID_T entityId : cell.items) {
+            EntityGridData& entity = getEntityData(entityId);
+            if (entity.queryID != query) {
+                entity.queryID = query;
+                m_resultCache.push_back(entityId);
+            }
+        }
+
+        if (cellX == endCell.x && cellY == endCell.y) {
+            break;
+        }
+
+        if (x < y) {
+            x += dirX;
+            cellX += gridDirX;
+            if (cellX < 0 || cellX >= m_gridSize) {
+                break;
+            }
+        } else {
+            y += dirY;
+            cellY += gridDirY;
+            if (cellY < 0 || cellY >= m_gridSize) {
+                break;
+            }
+        }
+    }
+
+    return m_resultCache;
+}
