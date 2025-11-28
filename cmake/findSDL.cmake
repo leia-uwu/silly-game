f0a7353e5a437e95b0518fcd4288c11c4b1c93c4
leia uwu <leia@tutamail.com>
2025-11-28 18:05:48 -0300

    chore: bump SDL

--- cmake/findSDL.cmake
+++ cmake/findSDL.cmake
@@ -4,7 +4,7 @@ function(findSDL)
         FetchContent_Declare(
             SDL
             GIT_REPOSITORY https://github.com/libsdl-org/SDL
-            GIT_TAG        release-3.2.22
+            GIT_TAG        release-3.2.26
             GIT_SHALLOW ON
         )
         FetchContent_MakeAvailable(SDL)
