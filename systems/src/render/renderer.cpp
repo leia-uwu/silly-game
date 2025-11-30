b2ece9005a0eef8a29c4f2ece982f8a5f724cefb
leia uwu <leia@tutamail.com>
2025-11-29 23:55:04 -0300

    perf: disable depth size

--- systems/src/render/renderer.cpp
+++ systems/src/render/renderer.cpp
@@ -59,7 +59,7 @@ SDL_AppResult Renderer::init()
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
     SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
     SDL_WindowFlags flags = SDL_WINDOW_OPENGL;
