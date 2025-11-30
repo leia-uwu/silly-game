0b1faf65f79997e99df2246f323ed218b182d415
leia uwu <leia@tutamail.com>
2025-11-29 23:56:33 -0300

    feat: rotate sprites on benchmark demo

--- examples/spritebenchmark/main.cpp
+++ examples/spritebenchmark/main.cpp
@@ -14,6 +14,7 @@ class BenchSprite : public SpriteItem
 public:
     BenchSprite() : SpriteItem() { };
     Vec2F velocity;
+    float rotationDir;
 };
 
 class Benchmark : public GameApp
@@ -43,6 +44,7 @@ public:
             sprite.position.y = renderer().windowHeight() / 2.F;
             sprite.velocity.x = ((std::rand() / (float)RAND_MAX) * 1000) - 500;
             sprite.velocity.y = ((std::rand() / (float)RAND_MAX) * 1000) - 500;
+            sprite.rotationDir = ((std::rand() / (float)RAND_MAX) * 20) - 10;
             root.addChild(&sprite);
         }
         renderer().setClearColor(0);
@@ -67,6 +69,8 @@ public:
             if (sprite.position.y < 0 || sprite.position.y > height) {
                 sprite.velocity.y = -sprite.velocity.y;
             }
+
+            sprite.rotation += sprite.rotationDir * dt;
 
             sprite.position.x = std::clamp(sprite.position.x, 0.F, (float)width);
             sprite.position.y = std::clamp(sprite.position.y, 0.F, (float)width);
