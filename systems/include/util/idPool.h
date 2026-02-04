9335b681602e5e47f3ad0f0dbd055e2e43d8158e
leia uwu <leia@tutamail.com>
2026-02-03 23:33:18 -0300

    feat: add idPool

---
+++ systems/include/util/idPool.h
@@ -0,0 +1,39 @@
+#include <list>
+#include <stdexcept>
+#include <type_traits>
+
+template<typename T>
+    requires(std::is_integral_v<T>)
+class IdPool
+{
+
+public:
+    IdPool(T maxID) : m_maxId(maxID)
+    {
+    }
+
+    [[nodiscard]] T getId()
+    {
+        if (m_nextId > m_maxId) {
+            if (m_freeList.empty()) {
+                throw std::runtime_error("Ran out of IDs!");
+            }
+            T id = m_freeList.back();
+            m_freeList.pop_back();
+            return id;
+        }
+
+        return m_nextId++;
+    }
+
+    void giveId(T id)
+    {
+        m_freeList.push_front(id);
+    }
+
+private:
+    T m_nextId = 1;
+    T m_maxId;
+
+    std::list<T> m_freeList;
+};
